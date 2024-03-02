db61962abc71efc826996157b2df3d0f55964f41
owolanab <owolanab@engr.orst.edu>
2024-03-02 14:50:09 -0600

    fix errors

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudfront_distribution" "s3_hosted_site_cloudfront_distro" {
   origin {
-    domain_name = "${var.hosted_site_name}.s3-website-us-east-1.amazonaws.com"
-    origin_id   = "${var.hosted_site_name}.s3-website-us-east-1.amazonaws.com"
+    domain_name = aws_s3_bucket.s3_hosted_site.bucket_regional_domain_name
+    origin_id   = "${var.hosted_site_name}"
   }
   enabled             = true
   is_ipv6_enabled     = true
@@ -15,119 +15,17 @@ resource "aws_cloudfront_distribution" "s3_hosted_site_cloudfront_distro" {
   }
   viewer_certificate {
     acm_certificate_arn = aws_acm_certificate.s3_hosted_site_cert.arn
+    ssl_support_method = "sni-only"
   }
   default_cache_behavior {
     viewer_protocol_policy = "redirect-to-https"
     cached_methods         = ["GET", "HEAD"]
     cache_policy_id        = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad" # Using the CachingDisabled managed policy ID:
     allowed_methods        = ["GET", "HEAD"]
-    target_origin_id       = "${var.hosted_site_name}.s3-website-us-east-1.amazonaws.com"
+    target_origin_id       = "${var.hosted_site_name}"
   }
   depends_on = [
     aws_acm_certificate.s3_hosted_site_cert,
     aws_s3_bucket.s3_hosted_site
   ]
 }
-
-
-# resource "aws_cloudfront_distribution" "s3_distribution" {
-#   origin {
-#     domain_name              = aws_s3_bucket.b.bucket_regional_domain_name
-#     origin_access_control_id = aws_cloudfront_origin_access_control.default.id
-#     origin_id                = local.s3_origin_id
-#   }
-
-#   enabled             = true
-#   is_ipv6_enabled     = true
-#   comment             = "Some comment"
-#   default_root_object = "index.html"
-
-#   logging_config {
-#     include_cookies = false
-#     bucket          = "mylogs.s3.amazonaws.com"
-#     prefix          = "myprefix"
-#   }
-
-#   aliases = ["mysite.example.com", "yoursite.example.com"]
-
-#   default_cache_behavior {
-#     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-#     cached_methods   = ["GET", "HEAD"]
-#     target_origin_id = local.s3_origin_id
-
-#     forwarded_values {
-#       query_string = false
-
-#       cookies {
-#         forward = "none"
-#       }
-#     }
-
-#     viewer_protocol_policy = "allow-all"
-#     min_ttl                = 0
-#     default_ttl            = 3600
-#     max_ttl                = 86400
-#   }
-
-#   # Cache behavior with precedence 0
-#   ordered_cache_behavior {
-#     path_pattern     = "/content/immutable/*"
-#     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-#     cached_methods   = ["GET", "HEAD", "OPTIONS"]
-#     target_origin_id = local.s3_origin_id
-
-#     forwarded_values {
-#       query_string = false
-#       headers      = ["Origin"]
-
-#       cookies {
-#         forward = "none"
-#       }
-#     }
-
-#     min_ttl                = 0
-#     default_ttl            = 86400
-#     max_ttl                = 31536000
-#     compress               = true
-#     viewer_protocol_policy = "redirect-to-https"
-#   }
-
-#   # Cache behavior with precedence 1
-#   ordered_cache_behavior {
-#     path_pattern     = "/content/*"
-#     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-#     cached_methods   = ["GET", "HEAD"]
-#     target_origin_id = local.s3_origin_id
-
-#     forwarded_values {
-#       query_string = false
-
-#       cookies {
-#         forward = "none"
-#       }
-#     }
-
-#     min_ttl                = 0
-#     default_ttl            = 3600
-#     max_ttl                = 86400
-#     compress               = true
-#     viewer_protocol_policy = "redirect-to-https"
-#   }
-
-#   price_class = "PriceClass_200"
-
-#   restrictions {
-#     geo_restriction {
-#       restriction_type = "whitelist"
-#       locations        = ["US", "CA", "GB", "DE"]
-#     }
-#   }
-
-#   tags = {
-#     Environment = "production"
-#   }
-
-#   viewer_certificate {
-#     cloudfront_default_certificate = true
-#   }
-# }
